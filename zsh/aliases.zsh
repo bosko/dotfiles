364cedf17e5691ab7a0ac6a21e5c75429152aa5d
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2012-11-04 21:12:34 +0100

    Alias for creating tags in Rails application

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias hidden-up='defaults write com.apple.Finder AppleShowAllFiles YES'
 alias hidden-down='defaults write com.apple.Finder AppleShowAllFiles NO'
+alias create-tags='ctags -e -R --exclude=db --exclude=log --exclude=tmp --exclude=test --exclude=.git --exclude=public --exclude=vendor'
