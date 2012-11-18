c9f992553c9953f388553f82be81bebae214ea9f
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2012-11-18 16:34:40 +0100

    Turn on Emacs bindings and change ctags alias for Rails project

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias hidden-up='defaults write com.apple.Finder AppleShowAllFiles YES'
 alias hidden-down='defaults write com.apple.Finder AppleShowAllFiles NO'
-alias create-tags='ctags -e -R --exclude=db --exclude=log --exclude=tmp --exclude=test --exclude=.git --exclude=public --exclude=vendor'
+alias tags-create='ctags -e -R --exclude=db --exclude=log --exclude=tmp --exclude=test --exclude=.git --exclude=public --exclude=vendor'
