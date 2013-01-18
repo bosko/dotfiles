b32297c0017c86ecbf27d75158baf62234b526e4
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2013-01-18 14:41:22 +0100

    Use my Git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,7 +8,10 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %C(bold blue)%an:%Creset %s -%Creset%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+# Pretty!
+# via http://andyjeffries.co.uk/articles/25-tips-for-intermediate-git-users
+alias glol='git log --pretty=oneline --abbrev-commit --graph --decorate'
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
