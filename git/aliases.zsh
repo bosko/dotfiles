79328cdf1863e680eb4ad1d0ae5ce99ee2f1d663
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2013-07-23 12:26:15 +0200

    Alias for pushing three main branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,3 +20,5 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gp-all='git push origin master && git push origin staging && git push origin development'
+
