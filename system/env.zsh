bd8a3510db5d3b90ea205376cba30730a5f05549
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2019-12-04 17:10:14 +0100

    feat: enable history in IEx

--- system/env.zsh
+++ system/env.zsh
@@ -3,3 +3,6 @@ export EDITOR='emacsclient -n $* -a emacs'
 # Improve Emacs colors when running inside terminal
 export LC_ALL=en_US.UTF-8
 export LANG=C
+
+# Enable Elixir/Erlang console history
+export ERL_AFLAGS="-kernel shell_history enabled"
