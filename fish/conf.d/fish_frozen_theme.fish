f25bba1451d5d4a350c639a510160b64090ff33b
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2026-07-30 14:22:19 +0200

    Set fish colors

--- fish/conf.d/fish_frozen_theme.fish
+++ fish/conf.d/fish_frozen_theme.fish
@@ -1,14 +1,11 @@
-# This file was created by fish when upgrading to version 4.3, to migrate
-# theme variables from universal to global scope.
 # Don't edit this file, as it will be written by the web-config tool (`fish_config`).
 # To customize your theme, delete this file and see
 #     help interactive#syntax-highlighting
 # or
 #     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
 # for appropriate commands to add to ~/.config/fish/config.fish instead.
-# See also the release notes for fish 4.3.0 (run `help relnotes`).
 
-set --global fish_color_autosuggestion b2b2b2
+set --global fish_color_autosuggestion ff5f00
 set --global fish_color_cancel --reverse
 set --global fish_color_command blue
 set --global fish_color_comment red
@@ -31,11 +28,11 @@ set --global fish_color_search_match --background=111
 set --global fish_color_selection white --bold --background=brblack
 set --global fish_color_status red
 set --global fish_color_user brgreen
-set --global fish_color_valid_path --underline
+set --global fish_color_valid_path --underline=single
 set --global fish_pager_color_background
 set --global fish_pager_color_completion normal
 set --global fish_pager_color_description B3A06D --italics
-set --global fish_pager_color_prefix cyan --bold --underline
+set --global fish_pager_color_prefix cyan --bold --underline=single
 set --global fish_pager_color_progress brwhite --background=cyan
 set --global fish_pager_color_secondary_background
 set --global fish_pager_color_secondary_completion
